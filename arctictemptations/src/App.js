f242a139db8d44bc23f772f5fc987a2c7260b435
robinofverberg <robin.ofverberg@gmail.com>
2023-10-18 13:59:22 +0200

    made project react

---
+++ arctictemptations/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Homepage" style={{ width: 1920, height: 1080, position: 'relative', background: 'linear-gradient(90deg, #F49999 0%, #FEC6C6 82%, #F49999 99%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+      <img className="Glass1" style={{ width: 665, height: 941, left: -6, top: 139, position: 'absolute' }} src="https://via.placeholder.com/665x941" />
+      <div className="FreezeTheMomentSavorTheTaste" style={{ width: 724, height: 96, left: 1156, top: 81, position: 'absolute', textAlign: 'center', color: '#DE6464', fontSize: 96, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Freeze the Moment, Savor the taste.</div>
+      <div className="Navbar" style={{ width: 1007, height: 248, left: 76, top: 15, position: 'absolute' }}>
+        <div className="Home" style={{ left: 0, top: 42, position: 'absolute', textAlign: 'center', color: '#DE6464', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Home</div>
+        <div className="AboutUs" style={{ left: 399, top: 42, position: 'absolute', textAlign: 'center', color: '#FFE4E4', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>About us</div>
+        <div className="OurProducts" style={{ left: 144, top: 42, position: 'absolute', textAlign: 'center', color: '#FFE4E4', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Our products</div>
+        <div className="Contact" style={{ left: 590, top: 42, position: 'absolute', textAlign: 'center', color: '#FFE4E4', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Contact</div>
+        <img className="Arcticlogo1" style={{ width: 247, height: 248, left: 760, top: 0, position: 'absolute', boxShadow: '0px 4px 4px -20px #D15D5D' }} src="https://via.placeholder.com/247x248" />
+      </div>
+      <div className="TheMomentIStepInsideArcticTemptationsMakesMeFeelLikeAKidAgain" style={{ width: 724, height: 152, left: 1156, top: 531, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>"The moment I step inside, Arctic Temptations makes me feel like a kid again."</div>
+      <div className="IVeFoundMyHappyPlaceAndItSCalledArcticTemptations" style={{ width: 724, height: 70, left: 1156, top: 696, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>"I've found my happy place, and it's called Arctic Temptations!"</div>
+      <div className="ICanTResistTheirIceCreamItSAWeeklyRitualForMe" style={{ width: 724, height: 50, left: 1156, top: 860, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>"I can't resist their ice cream – it's a weekly ritual for me."</div>
+      <div className="Knapp" style={{ width: 262, height: 82, left: 836, top: 950, position: 'absolute' }}>
+        <div className="Rectangle1" style={{ width: 262, height: 82, left: 0, top: 0, position: 'absolute', background: '#DE6464', borderRadius: 20 }} />
+        <div className="OurProducts" style={{ left: 33, top: 17, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Our Products</div>
+      </div>
+      <div className="Open95EveryDay" style={{ left: 639, top: 403, position: 'absolute', textAlign: 'center', color: '#513030', fontSize: 48, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Open 9-5 every day </div>
+      <div className="SveddvGen9a75652Uppsala" style={{ left: 685, top: 483, position: 'absolute', textAlign: 'center', color: '#513030', fontSize: 48, fontFamily: 'Modak', fontWeight: '400', wordWrap: 'break-word' }}>Sveddvägen 9A<br />756 52 Uppsala </div>
+    </div>
+  );
+}
+
+export default App;
